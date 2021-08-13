3e4f116f182efded784bc9d178712dac645de7df
gbfrid <gabefridkis@gmail.com>
2021-08-13 12:41:02 -0500

    changed API

--- client/components/screens/Home.js
+++ client/components/screens/Home.js
@@ -3,64 +3,107 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { connect } from "react-redux";
 import { fetchItem } from "../../store/item";
-import { db } from '../../../config.js'
-
+import { db } from "../../../config.js";
 
 class Home extends React.Component {
-  constructor({ route, navigation }){
-    super()
+  constructor({ route, navigation }) {
+    super();
     this.state = {
-      text: '',
-      carbs: '',
-      user: ''
-    }
+      text: "",
+      user: "",
+      nutrients: {
+        carbs: 0,
+        fiber: 0,
+        sugars: 0,
+        protein: 0,
+        fat: 0,
+      },
+    };
     this.handleChange = this.handleChange.bind(this);
     this.pressHandler = this.pressHandler.bind(this);
   }
-
 
   addToFirebase(usersRef, user) {
     usersRef.doc(user.id).set({
       name: user.name,
       firstName: user.givenName,
       email: user.email,
-      photoUrl: user.photoUrl
-    })
+      photoUrl: user.photoUrl,
+    });
   }
 
-
-
-
-
-
   async componentDidMount() {
-    const usersRef = db.collection('users')
+    const usersRef = db.collection("users");
     const { user } = this.props.route.params;
     this.setState({
       ...this.state,
-      user: user
-    })
+      user: user,
+    });
     try {
-    const fireBaseUser = (await usersRef.doc(user.id).get()).data()
-    if (!fireBaseUser) {
-      this.addToFirebase(usersRef, user)
-    }
-    } catch(e) {
-      console.log(e)
+      const fireBaseUser = (await usersRef.doc(user.id).get()).data();
+      if (!fireBaseUser) {
+        this.addToFirebase(usersRef, user);
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
+  // setNutrients(nutrientsList) {
+  //   nutrientsList.forEach((nutrient) => {
+  //     switch (nutrient.nutrientId) {
+  //       case 1005:
+  //         return this.setState({
+  //           ...this.state,
+  //           nutrients: {
+  //             ...this.state.nutrients,
+  //             carbs: nutrient.value,
+  //           },
+  //         });
+  //       case 2000:
+  //         return this.setState({
+  //           ...this.state,
+  //           nutrients: {
+  //             ...this.state.nutrients,
+  //             sugars: nutrient.value,
+  //           },
+  //         });
+  //       case 1079:
+  //         return this.setState({
+  //           ...this.state,
+  //           nutrients: {
+  //             ...this.state.nutrients,
+  //             fiber: nutrient.value,
+  //           },
+  //         });
+  //       case 1003:
+  //         return this.setState({
+  //           ...this.state,
+  //           nutrients: {
+  //             ...this.state.nutrients,
+  //             protein: nutrient.value,
+  //           },
+  //         });
+  //       case 1004:
+  //         return this.setState({
+  //           ...this.state,
+  //           nutrients: {
+  //             ...this.state.nutrients,
+  //             fat: nutrient.value,
+  //           },
+  //         });
+  //       default:
+  //         return 0;
+  //     }
+  //   });
+  // }
+
   async pressHandler() {
-    await this.props.fetchItem(this.state.text)
-    const item = this.props.item
-    const nutrientsList = item.foods[0].foodNutrients
-    nutrientsList.forEach(nutrient => {
-      if (nutrient.nutrientName === "Carbohydrate, by difference") {
-        this.setState({
-          carbs: nutrient.value
-        })
-      }
-    })
+    await this.props.fetchItem();
+    const item = this.props.item;
+    // const nutrientsList = item.foods[0].foodNutrients;
+    console.log(item);
+    // this.setNutrients(nutrientsList);
   }
 
   handleChange(textValue) {
@@ -73,51 +116,50 @@ class Home extends React.Component {
     return (
       <View style={styles.container}>
         <TextInput
-          name='text'
+          name="text"
           value={this.state.text}
-          style={{ fontSize: 42, color: 'steelblue' }}
+          style={{ fontSize: 42, color: "steelblue" }}
           placeholder="Enter A Food"
           onChangeText={this.handleChange}
         />
 
-        <Button
-          title={"Find Food"}
-          onPress={this.pressHandler}
-        />
-        <Text style={styles.text}>{`${this.state.carbs} Carbs`}</Text>
+        <Button title={"Find Food"} onPress={this.pressHandler} />
+        {/* <View>
+          <Text>{`${this.state.nutrients.carbs}g Carb`}</Text>
+          <Text>{`${this.state.nutrients.sugars}g Sugar`}</Text>
+          <Text>{`${this.state.nutrients.fiber}g Fiber`}</Text>
+          <Text>{`${this.state.nutrients.fat}g Fat`}</Text>
+        </View> */}
       </View>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
   text: {
-    color: 'rgb(59,108,212)',
-    fontSize: 42,
-    fontWeight: '100',
-    textAlign: 'center',
+    color: "rgb(59,108,212)",
+    fontSize: 20,
+    fontWeight: "100",
+    textAlign: "center",
   },
   container: {
     flex: 1,
     backgroundColor: "white",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
   },
-})
+});
 
 const mapState = (state) => {
   return {
-    item: state.item
-  }
-}
-
-
-const mapDispatch = (dispatch) => {
-  return {
-    fetchItem: (item) => dispatch(fetchItem(item))
+    item: state.item,
   };
 };
 
+const mapDispatch = (dispatch) => {
+  return {
+    fetchItem: (item) => dispatch(fetchItem(item)),
+  };
+};
 
 export default connect(mapState, mapDispatch)(Home);
--- client/store/item.js
+++ client/store/item.js
@@ -14,20 +14,34 @@ export const setItem = (item) => {
   };
 };
 
+const application_id = '58168d60';
+
+const application_key = '65a3322a4124fccabfce18b8b1ab7f8b';
+
+const food = 'apple'
 
 // THUNKS
-export const fetchItem = (item) => {
-  const params = {
-    api_key: 'p42NDXAu8L02EdMDNsKwnLcQOKYp1ZThq6IF8NsU',
-    query: item,
-    dataType: ["Survey (FNDDS)"],
-    pageSize: 1
-  }
-  const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${encodeURIComponent(params.query)}&pageSize=${encodeURIComponent(params.pageSize)}&api_key=${encodeURIComponent(params.api_key)}&dataType=${encodeURIComponent(params.dataType)}`
+export const fetchItem = () => {
+  // const params = {
+  //   api_key: 'p42NDXAu8L02EdMDNsKwnLcQOKYp1ZThq6IF8NsU',
+  //   query: item,
+  //   dataType: ["Survey (FNDDS)"],
+  //   pageSize: 1
+  // }
+  // const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${encodeURIComponent(params.query)}&pageSize=${encodeURIComponent(params.pageSize)}&api_key=${encodeURIComponent(params.api_key)}&dataType=${encodeURIComponent(params.dataType)}`
+
+  // const api_url = `https://trackapi.nutritionix.com/v2/search/instant?query=${encodeURIComponent(food)}?x-app-id=${encodeURIComponent(application_id)}&x-app-key=${encodeURIComponent(application_key)}`
   return async (dispatch) => {
     try {
-      const { data: item } = await axios.get(api_url);
+      const { data: item } = await axios.get(`https://trackapi.nutritionix.com/v2/search/instant?query=${food}`,
+        {
+        headers: {
+        "x-app-id": application_id,
+        "x-app-key": application_key,
+        },
+        });
       // const item = {item: 'cheese'}
+      console.log(item)
       dispatch(setItem(item));
     } catch (error) {
       console.log(error)
